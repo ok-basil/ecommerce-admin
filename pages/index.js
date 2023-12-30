01e74f1aec8cdefd9c746925f23fd96df9cdb593
Basil Okache <basilokache@gmail.com>
2023-12-30 14:40:35 +0100

    setting up authentication

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,21 @@ import { useSession, signIn, signOut } from "next-auth/react"
 export default function Home() {
   const { data: session } = useSession();
   if (!session) {
-    return 'not logged in';
-  }
-  return (
-    <div className="bg-black w-screen h-screen flex items-center">
-      <div className="text-center w-full">
-        <button className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
+    return (
+      <div className="bg-black w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
+        </div>
       </div>
-    </div>
-  )
+    );
+  }
+  else {
+    return (
+      <div className="bg-black w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
+        </div>
+      </div>
+    );
+  }
 }
