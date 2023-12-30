d99b870d7147519733634da521d676d832d9ca41
Basil Okache <basilokache@gmail.com>
2023-12-30 14:34:42 +0100

    setting up auth

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+
 export default function Home() {
+  const { data: session } = useSession();
+  if (!session) {
+    return 'not logged in';
+  }
   return (
     <div className="bg-black w-screen h-screen flex items-center">
       <div className="text-center w-full">
