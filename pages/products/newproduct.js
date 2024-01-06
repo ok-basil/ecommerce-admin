f7a82e31022eb073f006411ae484bc25c91b7a90
Basil Okache <basilokache@gmail.com>
2024-01-06 21:17:40 +0100

    new product page

--- pages/products/newproduct.js
+++ pages/products/newproduct.js
@@ -3,7 +3,13 @@ import Layout from "@/components/Layout";
 function NewProduct() {
     return (
         <Layout>
+            <h1>New Product</h1>
+            <label>Product Name</label>
             <input type="text" placeholder="Product Name" />
+            <label>Description</label>
+            <textarea placeholder="Description"></textarea>
+            <label>Price (in USD)</label>
+            <input type="text" placeholder="Price" />
         </Layout>
     )
 }
