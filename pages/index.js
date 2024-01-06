a9ddfa79d0f6e34365aad517adfaa3ab533222ce
Basil Okache <basilokache@gmail.com>
2024-01-06 20:30:16 +0100

    working on the admin dashboard

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,12 @@ function Home() {
   return (
     <Layout>
       <div className="text-blue-900 flex justify-between">
-        <h2>Hello, {session?.user?.name}</h2>
-        <div className="flex bg-gray-300 gap-1 text-black">
+        <h2>Hello, <b>{session?.user?.name}</b></h2>
+        <div className="flex bg-gray-300 gap-1 text-black rounded-lg ">
           <img src={session?.user?.image} alt="" className="w-6 h-6"/>
-          {session?.user?.name}
+          <span className="px-2">
+            {session?.user?.name}
+          </span>
         </div>
       </div>
     </Layout>
