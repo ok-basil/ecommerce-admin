8d24ca0a8966fe941ae7c5d16d195b35eeca988e
Basil Okache <basilokache@gmail.com>
2024-01-06 20:25:56 +0100

    working on the admin user display

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 function Home() {
+  const {data: session} = useSession();
+  
   return (
     <Layout>
-      test
+      <div className="text-blue-900 flex justify-between">
+        <h2>Hello, {session?.user?.name}</h2>
+        <div className="flex bg-gray-300 gap-1 text-black">
+          <img src={session?.user?.image} alt="" className="w-6 h-6"/>
+          {session?.user?.name}
+        </div>
+      </div>
     </Layout>
   )
 }
