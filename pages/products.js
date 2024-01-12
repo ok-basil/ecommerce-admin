398a53d7e40092736d87dd7ae24c45e7eae3458f
Basil Okache <basilokache@gmail.com>
2024-01-12 19:00:46 +0100

    displaying products on dashboard

--- pages/products.js
+++ pages/products.js
@@ -2,6 +2,7 @@ import Layout from "@/components/Layout";
 import Link from "next/link";
 import { useEffect } from "react";
 import axios from "axios";
+import { useState } from "react";
 
 function Products() {
     const [products, setProducts] = useState([]);
@@ -13,6 +14,22 @@ function Products() {
     return (
         <Layout>
             <Link href={'/products/newproduct'} className="bg-blue-900 text-white px-2 py-1 rounded-md">Add new product</Link>
+            <table>
+                <thead>
+                    <tr>
+                        <td>Product Name</td>
+                        <td></td>
+                    </tr>
+                    <tbody>
+                        {products.map(product => (
+                            <tr>
+                                <td>{product.title}</td>
+                                <td></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </thead>
+            </table>
         </Layout>
     );
 }
