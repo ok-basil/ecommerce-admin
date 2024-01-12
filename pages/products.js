2d54dec28f8d3683c1fe1a3cd200b34879413b78
Basil Okache <basilokache@gmail.com>
2024-01-12 19:17:05 +0100

    displaying products on dashboard

--- pages/products.js
+++ pages/products.js
@@ -14,21 +14,21 @@ function Products() {
     return (
         <Layout>
             <Link href={'/products/newproduct'} className="bg-blue-900 text-white px-2 py-1 rounded-md">Add new product</Link>
-            <table>
+            <table className="basic">
                 <thead>
                     <tr>
                         <td>Product Name</td>
                         <td></td>
                     </tr>
-                    <tbody>
-                        {products.map(product => (
-                            <tr>
-                                <td>{product.title}</td>
-                                <td></td>
-                            </tr>
-                        ))}
-                    </tbody>
                 </thead>
+                <tbody>
+                    {products.map(product => (
+                        <tr>
+                            <td>{product.title}</td>
+                            <td></td>
+                        </tr>
+                    ))}
+                </tbody>
             </table>
         </Layout>
     );
